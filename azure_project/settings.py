2a3c5e71ce009325aabb6ce8d1016c0e87c455b4
nalin-pijitkamnerd <nalint22@gmail.com>
2026-02-05 16:29:17 +0700

    change url

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -108,7 +108,7 @@ import urllib.parse
 mongo_username = urllib.parse.quote_plus('Nalin')
 mongo_password = urllib.parse.quote_plus('Admin123')
 # MONGO_URI = 'mongodb://localhost:27017/'
-MONGO_URI = f'mongodb+srv://{mongo_username}:{mongo_password}@mongodb+srv://Nalin:<password>@8025-mongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
+MONGO_URI = f'mongodb+srv://{mongo_username}:{mongo_password}@Nalin-mongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
 MONGO_DB_NAME = 'django_store_reviews'
 
 # Password validation
